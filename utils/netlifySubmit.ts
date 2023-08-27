a40d354d70f2807bb0bc97a511acf210ed3b2a7d
Kimber Guidebeck <kguidebeck@basicagency.com>
2023-08-27 14:13:12 -0400

    another attempt

--- utils/netlifySubmit.ts
+++ utils/netlifySubmit.ts
@@ -11,7 +11,7 @@ const netlifySubmit = async (values: any) => {
       .join('&');
   };
 
-  return fetch('/', {
+  return fetch('/static-form.html', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
